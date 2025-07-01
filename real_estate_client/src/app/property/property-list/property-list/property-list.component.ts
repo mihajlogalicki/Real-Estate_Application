4b2af26e3f7e06d23d2a40f2ae7f9427e9499230
mihajlogalicki <mihajloo966@gmail.com>
2025-07-02 00:19:41 +0200

    Setting up Angular including UI, components, folder structure

---
+++ real_estate_client/src/app/property/property-list/property-list/property-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-list',
+  standalone: false,
+  templateUrl: './property-list.component.html',
+  styleUrl: './property-list.component.css'
+})
+export class PropertyListComponent {
+
+}
