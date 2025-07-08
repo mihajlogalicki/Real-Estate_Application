9e76b35059efa630984ca98d10ffbca381edf653
mihajlogalicki <mihajloo966@gmail.com>
2025-07-08 23:47:21 +0200

    Added httpClient, rxjs, service and GET request for observable

--- real_estate_client/src/app/app.module.ts
+++ real_estate_client/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { PropertyCardComponent } from "./property/property-card/property-card.component";
 import { PropertyListComponent } from './property/property-list/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { provideHttpClient } from '@angular/common/http';
+import { HousingService } from './services/housing.service';
+
 
 @NgModule({
   declarations: [
@@ -16,9 +19,12 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule
 ],
-  providers: [],
+  providers: [
+    provideHttpClient(),
+    HousingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- real_estate_client/src/app/property/property-list/property-list/property-list.component.ts
+++ real_estate_client/src/app/property/property-list/property-list/property-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { take } from 'rxjs';
+import { HousingService } from '../../../services/housing.service';
 
 @Component({
   selector: 'app-property-list',
@@ -8,60 +10,25 @@ import { Component } from '@angular/core';
 })
 export class PropertyListComponent {
 
-  properties: Array<any> = [
-      {
-        "Id": 1,
-        "Name": "Sunset Ridge Villa",
-        "Type": "Villa",
-        "Price": 700000
-      },
-      {
-        "Id": 2,
-        "Name": "The Summit House",
-        "Type": "House",
-        "Price": 90000
-      },
-      {
-        "Id": 3,
-        "Name": "Ocean Breeze Villa",
-        "Type": "Villa",
-        "Price": 450000
-      },
-      {
-        "Id": 4,
-        "Name": "Bluebell Bungalow",
-        "Type": "Bungalow",
-        "Price": 50000
-      },
-      {
-        "Id": 5,
-        "Name": "Pinehill Residence",
-        "Type": "Residence",
-        "Price": 900000
-      },
-      {
-        "Id": 6,
-        "Name": "Silverleaf Residence",
-        "Type": "House",
-        "Price": 250000
-      },
-      {
-        "Id": 7,
-        "Name": "Stonegate House",
-        "Type": "House",
-        "Price": 100000
-      },
-      {
-        "Id": 8,
-        "Name": "Villa Toscana",
-        "Type": "Villa",
-        "Price": 150000
-      },
-      {
-        "Id": 8,
-        "Name": "The Lavender Lane House",
-        "Type": "House",
-        "Price": 60000
-      }
-  ];
+  properties: Array<any>;
+
+  constructor(private housingService: HousingService){}
+
+  ngOnInit() : void {
+    this.getProperties();
+  }
+
+  getProperties(){ 
+    this.housingService.getAllProperties()
+        .pipe(take(1))
+        .subscribe({
+         next: data => {
+            this.properties = data;
+        },
+         error: () => {
+            console.log(`Server error occured`);
+        }
+    })
+  }
+
 }
